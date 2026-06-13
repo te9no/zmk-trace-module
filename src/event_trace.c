edd418d75db8be0458ecf7be247a0018e9eb17e9
te9no <tatuya9428@gmail.com>
2026-06-13 22:16:35 +0900

    Fix compatibility with SAA ZMK base

--- src/event_trace.c
+++ src/event_trace.c
@@ -30,9 +30,8 @@ static int zmk_trace_listener(const zmk_event_t *eh) {
     const struct zmk_layer_state_changed *layer = as_zmk_layer_state_changed(eh);
     if (layer != NULL) {
         char data[96];
-        snprintk(data, sizeof(data), "{\"layer\":%u,\"state\":%s,\"locked\":%s,\"timestamp\":%lld}",
-                 layer->layer, layer->state ? "true" : "false",
-                 layer->locked ? "true" : "false", (long long)layer->timestamp);
+        snprintk(data, sizeof(data), "{\"layer\":%u,\"state\":%s,\"timestamp\":%lld}",
+                 layer->layer, layer->state ? "true" : "false", (long long)layer->timestamp);
         zmk_trace_emit_event("layer_state_changed", data);
         return ZMK_EV_EVENT_BUBBLE;
     }
